ae9a5eb458cc073c73c5d9f67edf8a989571a7af
pavel <pmykhaleiko@gmail.com>
2020-02-06 22:11:37 +0200

    Updates

---
+++ precache-manifest.7ef3f2e070015782737c3b7143282d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305be0ef6567d03a792d7575c0a36b78",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8806e0e210f67d2fd0a5",
+    "url": "/todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "935201568e444939124d",
+    "url": "/todo-app/static/js/2.f5e2bbac.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-app/static/js/2.f5e2bbac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8806e0e210f67d2fd0a5",
+    "url": "/todo-app/static/js/main.efdb10a5.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
